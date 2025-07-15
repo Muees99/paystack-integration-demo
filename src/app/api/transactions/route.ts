7fdb35e5a00c8a461f0a09ec8e5a5df853b11019
Muees99 <132849693+Muees99@users.noreply.github.com>
2025-07-15 15:37:50 +0100

    api-transactons-routes

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -1,28 +1,3 @@
-// // app/api/transactions/route.ts
-// import { NextResponse } from "next/server";
-
-// export async function GET() {
-//   const PAYSTACK_SECRET = process.env.PAYSTACK_SECRET_KEY;
-
-//   const res = await fetch("https://api.paystack.co/transaction", {
-//     headers: {
-//       Authorization: `Bearer ${PAYSTACK_SECRET}`,
-//       "Content-Type": "application/json",
-//     },
-//   });
-
-//   const data = await res.json();
-
-//   if (!res.ok) {
-//     return NextResponse.json(
-//       { error: data.message || "Failed to fetch transactions" },
-//       { status: 500 }
-//     );
-//   }
-
-//   return NextResponse.json(data.data); // Only return the array of transactions
-// }
-
 // app/api/transactions/route.ts
 import { NextResponse } from "next/server";
 import { getLoggedTransactions } from "@/libs/logTransaction";
